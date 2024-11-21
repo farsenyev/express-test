31f404f6309dfe39e91b4035271d8624f0f638a2
farsenyev <farsenyev@bk.ru>
2024-11-22 00:37:43 +0300

    update vercel settings

--- app.js
+++ app.js
@@ -21,4 +21,9 @@ app.listen(port, () => {
     console.log(`Сервер запущен на http://localhost:${port}`);
 });
 
+app.use((req, res, next) => {
+    console.log(`Request: ${req.method} ${req.url}`);
+    next();
+});
+
 module.exports = app;
